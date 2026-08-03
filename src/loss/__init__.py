ccdc7a021701194c3f676fbeecc6cb7b45f800d6
luannt1010 <luanntai13@gmail.com>
2026-08-03 16:27:23 +0700

    chore: add package init file

---
+++ src/loss/__init__.py
@@ -0,0 +1,5 @@
+from .ada_face_loss import AdaFaceLoss
+from .arc_face_loss import ArcFaceLoss
+from .batch_hard_triplet_loss import BatchHardTripletLoss
+
+__all__ = ["ArcFaceLoss", "AdaFaceLoss", "BatchHardTripletLoss"]
