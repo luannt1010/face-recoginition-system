a88a2f6745825dbcbf8664f55c567f23b855e490
luannt1010 <luanntai13@gmail.com>
2026-08-04 17:22:51 +0700

    chore: add init MobileFaceNet

--- src/net/__init__.py
+++ src/net/__init__.py
@@ -1,4 +1,5 @@
 from .iresnet_ecnoder import IResNetEncoder
 from .simple_resnet_encoder import SimpleResNet
+from .mobilenet_encoder import MobileFaceNet
 
-__all__ = ["IResNetEncoder", "SimpleResNet"]
+__all__ = ["IResNetEncoder", "SimpleResNet", "MobileFaceNet"]
