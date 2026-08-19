ba824687e6520f916ced8becc4a837d3afe0e000
luannt1010 <luanntai13@gmail.com>
2026-08-19 13:03:45 +0700

    chore: update init

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
-from .helper import (create_data_splits, crop_face, define_transform, extract_embedding,
-                           face_verification, load_model, plot_history, train, create_loss)
+from .helper import (create_data_splits, crop_face, define_transform, extract_embedding, get_model_size, evaluate, 
+                     face_verification, load_model, plot_history, train, create_loss, export_onnx, measure_average_inference_time_ms)
 
-__all__ = ["create_data_splits", "crop_face", "define_transform", "extract_embedding", "face_verification",
-           "load_model", "plot_history", "train", "create_loss"]
+__all__ = ["create_data_splits", "crop_face", "define_transform", "extract_embedding", "face_verification", "evaluate",
+           "load_model", "plot_history", "train", "create_loss", "export_onnx", "get_model_size", "measure_average_inference_time_ms"]
