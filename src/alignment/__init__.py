4ba99e91181d905a2b85b8913d2d0e4dfb2cbe65
luannt1010 <luanntai13@gmail.com>
2026-08-19 13:02:36 +0700

    chore: update init

--- src/alignment/__init__.py
+++ src/alignment/__init__.py
@@ -1,3 +1,3 @@
-from .align import calculate_area, calculate_center_dist, validate_face_pose, return_landmark, draw_img
+from .align import calculate_area, calculate_center_dist, validate_face_pose, return_landmark, draw_img, align_img
 
-__all__ = ["calculate_area", "calculate_center_dist", "validate_face_pose", "return_landmark", "draw_img"]
+__all__ = ["calculate_area", "calculate_center_dist", "validate_face_pose", "return_landmark", "draw_img", "align_img"]
