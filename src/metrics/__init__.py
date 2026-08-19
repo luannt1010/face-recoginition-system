8557fed18a0e1c7c9222bcfb2adc0ea79281040e
luannt1010 <luanntai13@gmail.com>
2026-08-19 13:03:25 +0700

    chore: update init

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -1,5 +1,5 @@
 from .metrics import (calculate_cosine_similarity, calculate_euclid_distance, classification_metrics,
-                      l2_normalize, verification_metrics_report)
+                      l2_normalize, verification_metrics_report, plot_roc_curve)
 
 __all__ = ["calculate_cosine_similarity", "calculate_euclid_distance", "classification_metrics",
-           "l2_normalize", "verification_metrics_report"]
+           "l2_normalize", "verification_metrics_report", "plot_roc_curve"]
